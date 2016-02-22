ec73cfe5a7f8253ec4e3918a609721cd1f33a6ae
Valter Lima Mesquita <limavalter@gmail.com>
2016-02-22 15:01:43 -0300

    May now be working

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,15 @@ var shortener = require('../lib/shortener.js')
 
 /* GET home page. */
 router.get('/:param', function(req, res, next) {
-	var response = shortener.shortify(req.params.param, req.headers.host)
-	console.log(response);
-	if(response.redir){
-		var redir = 'http://'+response.original_url;
-		console.log(redir)
-		res.redirect(redir);
-	}
-	else{
-		res.send(response);
-	}
+  var response = shortener.shortify(req.params.param, req.headers.host)
+  
+  if(response.redir){
+    var redir = 'http://'+response.original_url;
+    res.redirect(redir);
+  }
+  else{
+    res.send(response);
+  }
 });
 
 module.exports = router;
